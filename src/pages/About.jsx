0d9afa7a1fbaf8b6c6a9ad72c848bc918bdaa553
Dinesh Kumar Kannan <dineshmail.kannan@gmail.com>
2025-07-25 19:58:50 +0530

    spell correct

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -14,7 +14,7 @@ export default function About() {
           <p>
             Our mission is to enliven your road trip with the perfect travel van
             rental. Our vans are recertified before each trip to ensure your
-            travel plans can go off without a hitch. (Hitch costs extra 😉)
+            travel plans can go off without a hitch. (Hitch costs extra's 😉)
           </p>
           <p>
             Our team is full of vanlife enthusiasts who know firsthand the magic
